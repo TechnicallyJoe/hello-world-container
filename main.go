d3cc75fcb302f10e1bbbeac757e61692ae86f35b
Jonathan Østrup <joos@baselifescience.com>
2022-03-02 02:12:13 +0100

    added a /timedkill that kills the app after 60 sec

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -18,12 +19,14 @@ func main() {
 
 	sleepCounter, _ := strconv.Atoi(os.Getenv("HWC_SLEEPTIMER"))
 
+	// increments sleeptimer, starting from HWC_SLEEPTIMER
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello for the %d time \n", sleepCounter)
 		fmt.Printf("Hello for the %d time \n", sleepCounter)
 		sleepCounter++
 	})
 
+	// Prints environment variables
 	http.HandleFunc("/env", func(w http.ResponseWriter, r *http.Request) {
 		for _, v := range os.Environ() {
 			fmt.Fprintf(w, "%s \n", v)
@@ -31,8 +34,15 @@ func main() {
 		}
 
 	})
-
+	// Kills the service
 	http.HandleFunc("/kill", func(w http.ResponseWriter, r *http.Request) {
+		os.Exit(5)
+	})
+	// Kills the service after 60 sec
+	http.HandleFunc("/timedkill", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This service will be killed in 60 seconds \n")
+		fmt.Printf("This service will be killed in 60 seconds \n")
+		time.Sleep(60 * time.Second)
 		os.Exit(5)
 	})
 
